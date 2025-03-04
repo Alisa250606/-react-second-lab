369ecde25592b6dd4fc1aba6eaec1ae3b3707eb4
Alisa250606 <kn23-a.daieva@nubip.edu.ua>
2025-03-04 11:18:22 +0200

    Lab 12 complete.

--- my-app/src/components/Card/Card.jsx
+++ my-app/src/components/Card/Card.jsx
@@ -8,6 +8,9 @@ const Card = ({ title, description, image }) => {
       <div className="card-content">
         <h3 className="card-title">{title}</h3>
         <p className="card-description">{description}</p>
+        <button className="card-button" onClick={()=>{
+          alert(title)
+        }}>Дізнатися більше</button>
       </div>
     </div>
   );
